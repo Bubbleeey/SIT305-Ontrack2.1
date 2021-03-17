f2ea4dec70726d4725ae4f7facb8ec3d1e937f72
Jayden Ellisdon <jellisdon@deakin.edu.au>
2021-03-17 19:53:37 +1100

    Got spinner to work in if statment on button

--- Ontrack 2.1/app/src/main/java/com/example/ontrack21/MainActivity.java
+++ Ontrack 2.1/app/src/main/java/com/example/ontrack21/MainActivity.java
@@ -7,13 +7,43 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    //declared variables
+    Button button1;
+    Button button2;
+    Button button3;
+    EditText input;
+    TextView output1name;
+    TextView output1;
+    TextView output2name;
+    TextView output2;
+    TextView output3name;
+    TextView output3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assigns all variables to their ui elements
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        input = findViewById(R.id.input);
+        output1name = findViewById(R.id.output1name);
+        output1 = findViewById(R.id.output1);
+        output2name = findViewById(R.id.output2name);
+        output2 = findViewById(R.id.output2);
+        output3name = findViewById(R.id.output3name);
+        output3 = findViewById(R.id.output3);
 
         //creates spinner array and applys it to the spinner
         Spinner spinner = findViewById(R.id.spinner);
@@ -28,18 +58,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         //Where Button on click functions go
         //and inside each button if statement to adapit to which one is selected in array
-        if(parent.getItemAtPosition(position).toString() == "Meter")
-        {
-
-        }
-        if(parent.getItemAtPosition(position).toString() == "Celsius")
-        {
-
-        }
-        if(parent.getItemAtPosition(position).toString() == "Kilograms")
-        {
-
-        }
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(parent.getSelectedItem().toString().equals("Metre"))
+                {
+                    output1.setText("1");
+                }
+                if(parent.getSelectedItem().toString().equals("Celsius"))
+                {
+                    output1.setText("2");
+                }
+                if(parent.getSelectedItem().toString().equals("Kilograms"))
+                {
+                    output1.setText("3");
+                }
+            }
+        });
     }
 
     @Override
